eca2224c2d34a747cf63a4d1746961c0d8a0f382
davalenzblanc <davalenzblanc@gmail.com>
2024-05-09 22:28:13 -0500

    setup backend endpoints to get products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://ojrmwfr769.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ojrmwfr769.execute-api.us-east-1.amazonaws.com",
