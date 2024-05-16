0bb6930f3b6a35263de8fa49e27efe00547b1b76
davalenzblanc <davalenzblanc@gmail.com>
2024-05-16 09:41:14 -0500

    change import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ojrmwfr769.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://41sukuvuhh.execute-api.sa-east-1.amazonaws.com/import",
   bff: "https://ojrmwfr769.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
